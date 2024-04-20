d463fc7621ca0b4e9f1b7e35b3f2858df8be7fc5
Kisjolanka01 <lytwynd08@gmail.com>
2024-04-20 11:12:55 +0200

    as

--- Python/inputfeldolgozo.py
+++ Python/inputfeldolgozo.py
@@ -1,13 +1,13 @@
-file=open('python/input.txt')
+file=open('Python/input.txt')
 sorok=file.readlines()
 file.close()
 
 szamok = []
 for i in range(len(sorok)):
-    szamok.append(sorok[i].strip())
+    szamok.append(int(sorok[i].strip))
 
-osszeg=[]
+osszeg=0
 for i in range(len(szamok)):
-    osszeg+=szamok[i]
-atlag = osszeg/len(szamok)
-print(f'elso file atlaga: {atlag}')
+     osszeg+=szamok[i]
+atlag=osszeg/len(szamok)
+print(F"az elso file atlaga: {atlag}")
